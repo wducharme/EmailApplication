222411f7d3fc440db9aa4e33f924659a4c65cc91
Will DuCharme <wducharme@usc.edu>
2018-06-14 15:53:09 -0700

    Initial Commit

---
+++ Webscrape TutorialTests/Webscrape_TutorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  Webscrape_TutorialTests.swift
+//  Webscrape TutorialTests
+//
+//  Created by Will Ducharme on 6/14/18.
+//  Copyright © 2018 DuCharme. All rights reserved.
+//
+
+import XCTest
+@testable import Webscrape_Tutorial
+
+class Webscrape_TutorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
